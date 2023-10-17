21e67c6f403d0fee86541f0119f1705c30597789
Mckgare0717 <mckinnonrebello@gmail.com>
2023-10-17 16:37:00 +0100

    blogs updates

--- src/views/blog.js
+++ src/views/blog.js
@@ -3,6 +3,7 @@ import Health from "../components/health.js"
 import pic1 from "../assests/balls.jpg"
 
 const Blog = ()=>{
+
     return(
         <>
         <h2 className="blog">Blogs</h2>
@@ -17,7 +18,13 @@ const Blog = ()=>{
                 <Health image={pic1} link="https://www.nerdfitness.com/blog/" descp="The 20 Minute Beginner Kettlebell Workout: Try This Simple Workout at Home or Anywhere!"/>
                 <Health image={pic1} link="https://www.nerdfitness.com/blog/" descp="The 20 Minute Beginner Kettlebell Workout: Try This Simple Workout at Home or Anywhere!"/>
                 <Health image={pic1} link="https://www.nerdfitness.com/blog/" descp="The 20 Minute Beginner Kettlebell Workout: Try This Simple Workout at Home or Anywhere!"/>
-
+        </div>
+        <div className="cust-cont">
+            <h2>Customise your workout plan as per your needs</h2>
+            <form>
+                <h2>Enter Weight</h2>
+                <input type="text" value="weight" placeholder="Enter Weight"/>
+            </form>
         </div>
         </>
     )
